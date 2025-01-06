3f6ab3650513aa1a5534c2e6cec086fa4a412dc6
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-06 19:37:47 +0530

    fix syntax error path

--- src/main/java/com/cinnamon/controller/CartController.java
+++ src/main/java/com/cinnamon/controller/CartController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("carts")
+@RequestMapping("/carts")
 @RequiredArgsConstructor
 public class CartController {
 
