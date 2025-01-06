7542636f3e200df19f782011045a23327599c1c9
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-06 13:08:39 +0530

    create Cart model issue fix

--- src/main/java/com/cinnamon/model/Cart.java
+++ src/main/java/com/cinnamon/model/Cart.java
@@ -1,16 +1,13 @@
 package com.cinnamon.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.*;
 
-@Entity
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @ToString
-@Table(name = "carts")
 public class Cart {
     private Long id;
     private Long userId;
---
+++ src/main/java/com/cinnamon/service/impl/CartServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cinnamon.service.impl;
+
+import com.cinnamon.service.CartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl implements CartService {
+}
